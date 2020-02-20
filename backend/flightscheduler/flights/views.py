a226d74e6fcc5cd4dc69be3dca8478c0c5eada55
amir <amirfathi8888@gmail.com>
2020-02-21 03:00:29 +0330

    create db

--- backend/flightscheduler/flights/views.py
+++ backend/flightscheduler/flights/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from flights.serializers import UserSerializer
-from rest_framework import viewsets
-# Create your views here.
+from flights.models import Schedule
+from flights.serializers import UserSerializer, ScheduleSerializer
+from rest_framework import viewsets, status
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 def index(request):
@@ -13,3 +17,58 @@ def index(request):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+#APIS
+# /flight/
+
+@csrf_exempt
+def flight_list(request):
+    #get all
+    if request.method == 'GET':
+        schedules = Schedule.objects.all()
+        schedules_serializer = ScheduleSerializer(schedules,many=True)
+        return JsonResponse(schedules_serializer.data, safe=False)
+
+    #Add one
+    if request.method == 'POST':
+        schedule_data = JSONParser().parse(request)
+        schedule_serializer = ScheduleSerializer(data=schedule_data)
+        if schedule_serializer.is_valid():
+            schedule_serializer.save()
+            return JsonResponse(schedule_serializer.data, status=status.HTTP_201_CREATED)
+
+        return JsonResponse(schedule_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        Schedule.objects.all().delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
+@csrf_exempt
+def flight_detail(request, pk):
+    try:
+        schedule = Schedule.objects.get(pk=pk)
+    except Schedule.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+
+    #Retrive one
+    if request.method == 'GET':
+        schedule_serializer = ScheduleSerializer(schedule)
+        return  JsonResponse(schedule_serializer.data)
+
+    #Update one
+    if request.method == 'PUT':
+        schedule_data = JSONParser().parse(request)
+        schedule_serializer = ScheduleSerializer(schedule, data = schedule_data)
+        if schedule_serializer.is_valid():
+            schedule_serializer.save()
+            return JsonResponse(schedule_serializer.data)
+        return JsonResponse(schedule_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #Delete one record
+    if request.method == 'DELETE':
+        schedule.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
