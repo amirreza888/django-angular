5f4b279dc71c75ce05991111ec8d92218768845f
amir <amirfathi8888@gmail.com>
2020-02-21 18:03:40 +0330

    anular added

--- backend/flightscheduler/flightscheduler/settings.py
+++ backend/flightscheduler/flightscheduler/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.CorsMiddleware'
+    #'django.middleware.CorsMiddleWare'
 ]
 
 CORS_ORIGIN_WHITELIST = [
