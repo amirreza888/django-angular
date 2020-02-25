44d35cab808a1d5bb4299a5bec743c70af441129
amir <amirfathi8888@gmail.com>
2020-02-25 23:19:19 +0330

    viste a bug

--- frontend/flight-scheduler/src/app/app.module.ts
+++ frontend/flight-scheduler/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+//  import {} from './app-routing.module';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FlightListComponent } from './flight-list/flight-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
